d7a6a457ead67391553ba80e6d1dac600d5a0c9c
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-06-11 07:25:36 -0400

    Returns artist/album info successfully

--- liri.js
+++ liri.js
@@ -1,7 +1,9 @@
 var keys = require("./keys.js");
 var twitter = require("twitter");
+var spotify = require("spotify");
 
 var command = process.argv[2];
+var title = process.argv[3];
 
 if(command == "my-tweets"){
 	console.log("tweet tweet");
@@ -18,19 +20,29 @@ if(command == "my-tweets"){
 		}
 		else{
 			console.log("");
+
 			for(i = 0; i < 20; i++){
 				console.log(tweets[i].text);
 				console.log(tweets[i].created_at);
 				console.log("");
 			}
-				// console.log(tweets.text);
-
 		}
 	});
-	// console.log(keys.twitterKeys.consumer_key);
 }
+
 else if(command == "spotify-this-song"){
 	console.log("down for some tunage");
+
+	spotify.search({type: "track", query: "tom sawyer"}, function(error, data){
+		if(error){
+			console.log(error);
+		}
+		else{
+				console.log(data.tracks.items[0].name);
+				console.log(data.tracks.items[0].artists[0].name);
+				console.log(data.tracks.items[0].album.name);
+		}
+	});
 }
 else if(command == "movie-this"){
 	console.log("let's go to the movies, shawtay");
