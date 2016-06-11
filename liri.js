4758ac2b072301a02e8c9020447d1aa536f02807
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-06-11 06:26:52 -0400

    Twitter commands work successfully; produces last 20 tweets and timestamp

--- liri.js
+++ liri.js
@@ -1,10 +1,33 @@
 var keys = require("./keys.js");
+var twitter = require("twitter");
 
 var command = process.argv[2];
 
 if(command == "my-tweets"){
 	console.log("tweet tweet");
-	console.log(keys.twitterKeys.consumer_key);
+	var client = new twitter({
+		consumer_key: keys.twitterKeys.consumer_key,
+		consumer_secret: keys.twitterKeys.consumer_secret,
+		access_token_key: keys.twitterKeys.access_token_key,
+		access_token_secret: keys.twitterKeys.access_token_secret,
+	}); 
+
+	client.get("statuses/user_timeline", function(error, tweets, response){
+		if(error){
+			console.log(error);
+		}
+		else{
+			console.log("");
+			for(i = 0; i < 20; i++){
+				console.log(tweets[i].text);
+				console.log(tweets[i].created_at);
+				console.log("");
+			}
+				// console.log(tweets.text);
+
+		}
+	});
+	// console.log(keys.twitterKeys.consumer_key);
 }
 else if(command == "spotify-this-song"){
 	console.log("down for some tunage");
